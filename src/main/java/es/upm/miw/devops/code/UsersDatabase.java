0eaef393d17eebd5096f8a622120b02985c0ca82
Juliojld12 <119012467+Juliojld12@users.noreply.github.com>
2025-09-24 12:55:27 +0200

    add UsersDatabase and its test #7

---
+++ src/main/java/es/upm/miw/devops/code/UsersDatabase.java
@@ -0,0 +1,35 @@
+package es.upm.miw.devops.code;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class UsersDatabase {
+
+    private final List<User> users;
+
+    public UsersDatabase() {
+        this.users = new ArrayList<>(List.of(
+                new User("1", "Ada", "Lovelace"),
+                new User("2", "Alan", "Turing"),
+                new User("3", "Grace", "Hopper"),
+                new User("4", "Linus", "Torvalds"),
+                new User("5", "Dennis", "Ritchie")
+        ));
+    }
+
+    public List<User> getUsers() {
+        return new ArrayList<>(this.users); // Evita modificación externa
+    }
+
+    public Optional<User> findUserById(String id) {
+        return this.users.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+    }
+
+    public Stream<User> stream() {
+        return this.users.stream();
+    }
+}
